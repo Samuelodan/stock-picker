80b0b1e8e4d194a303373ae4e412e3dc8555dd74
Samuel ODaniels <samodan195@gmail.com>
2022-05-03 14:24:56 +0100

    Replace until loop with for loop and remove unnecessary 'i' variable

--- main.rb
+++ main.rb
@@ -1,7 +1,6 @@
 def stock_picker(arr)
-  i = 0
   max_diff = arr[1] - arr[1]
-  until i == arr.length do
+  for i in 0...arr.length
     for j in (i + 1)...arr.length
       if arr[j] - arr[i] > max_diff
         max_diff = arr[j] - arr[i]
